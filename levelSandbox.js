d1e8084661491998f89439d09dd22c2beafb2324
Jens Göring <jeeens@jeeens.de>
2018-12-24 00:16:28 +0100

    changed to promises

--- levelSandbox.js
+++ levelSandbox.js
@@ -7,31 +7,56 @@ const chainDB = './chaindata';
 const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
-function addLevelDBData(key,value){
-  db.put(key, value, function(err) {
-    if (err) return console.log('Block ' + key + ' submission failed', err);
-  })
+function addLevelDBData(key, value) {
+    return new Promise(function (resolve, reject) {
+        db.put(key, value, err => {
+            if (err) {
+                console.log('Block' + key + ' submission failed', err);
+                reject(err);
+            } else {
+                resolve(value);
+            }
+        })
+    });
 }
 
 // Get data from levelDB with key
 function getLevelDBData(key){
-  db.get(key, function(err, value) {
-    if (err) return console.log('Not found!', err);
-    console.log('Value = ' + value);
-  })
+    return new Promise(function (resolve, reject) {
+        db.get(key, (err, value) => {
+            if (err) {
+                if (err.type === 'NotFoundError') {
+                    resolve('undefined');
+                } else {
+                    console.log('Block' + key + ' get failed', err);
+                    reject(err);
+                }
+            } else {
+                resolve(value);
+            }
+        });
+    });
 }
+
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
-    let i = 0;
-    db.createReadStream().on('data', function(data) {
-          i++;
-        }).on('error', function(err) {
-            return console.log('Unable to read data stream!', err)
-        }).on('close', function() {
-          console.log('Block #' + i);
-          addLevelDBData(i, value);
-        });
+
+    return new Promise(function (resolve, reject) {
+        let i = 0;
+        db.createReadStream()
+            .on('data', function (data) {
+                i++;
+            })
+            .on('error', function (err) {
+                console.log('Unable to read data stream!', err);
+                reject(err)
+            })
+            .on('close', function () {
+                console.log('Block #' + i);
+                resolve(addLevelDBData(i, value));
+            });
+    });
 }
 
 /* ===== Testing ==============================================================|
